ce765b89f3d9ceab57ee61f114924f917e374b84
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2023-06-30 18:10:06 +0200

    Empezamos a trabajar con los ficheros de texto

---
+++ FicherosTexto.cpp
@@ -0,0 +1,55 @@
+//Explicacion ficheros de texto
+//Librerias
+#include <iostream>
+#include <fstream> //Para trabajar la entrada/salida estandar con los ficheros de texto
+
+//Usos
+using namespace std;
+
+//Funciones
+
+
+//Funcion main
+int main()
+{
+    //Variables
+    string lineaFicheroTexto ="";
+
+    //clase para abrir el fichero en modo escritura primero la ruta donde se encuentra el texto y, para no machacar el texto se utiliza el append (ruta, append)
+    ofstream fs("./info/ficherocpp.txt", ofstream::out | ofstream::app);
+
+    //Para escribir utilizamos fs 
+    fs << "He escrito en la línea 1 con c++" << endl;
+    fs << "Esta es la segunda línea" <<endl;
+
+    //Una vez escrito, cerramos el fichero
+    fs.close();
+
+    //Clase para abrir el fichero en modo lectura
+    ifstream fr;
+    fr.open("./info/ficherocpp.txt");
+
+    //Tipico while para recorrer linea a linea, la funcion eof() significa fin de fichero
+    while ( !(fr.eof()) )
+    {
+        //getLine sirve para capturar las lineas del fichero getline(donde lo metes, variable para almacenar)
+        getline(fr, lineaFicheroTexto);
+        cout << lineaFicheroTexto << endl;
+    }
+
+    //Cerramos el fichero
+    fr.close();
+    
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
