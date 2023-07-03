38387d544ff8fec32ba1626a282ec0f80851716e
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2023-07-03 10:43:51 +0200

    Empezamos los ficheros binarios con dos practicas de escritura y lectura

---
+++ FicherosBinarios/FicherosBinarios.cpp
@@ -0,0 +1,50 @@
+//Tutorial sobre la gestion de los ficheros binarios en C++
+//Librerios
+#include <iostream>
+#include <fstream>
+
+//Usos
+using namespace std;
+
+//Funciones
+
+
+
+
+
+//Funcion principal
+int main()
+{
+    //Inicializamos las variables con las que vamos a trabajar
+    int i = 0;
+    int numeroLeido = 0;
+
+    //Para abrir el fichero en modo de escritura usamos, si el fichero no existe lo va a crear
+    fstream fout("./info/datos.dat", ios::binary | ios::out);
+
+    //Creamos un bucle para que esriba los numeros dentro del fichero
+    for ( i = 200; i <= 400; i++)
+    {
+        // el char * es la manera de acceder a los bytes dentro de un fichero, le indica una posicion en la memorio con el &i, 
+        // despues hay que indicarle el tamaño del dato con el tipo entre parentesis
+        fout.write((char *) &i, sizeof(int));
+    }
+    fout.close(); //Hay que cerrar el fichero como siempre que trabajamos con ficheros
+
+    //Ahora vamos a leer el fichero, cambiamos el ios::out por el ios::in para indicarles que es de entrada de datos
+    fstream fin("./info/datos.dat", ios::binary | ios::in);
+
+    //Creamos un bucle para que lea los numeros dentro del fichero
+    for ( i = 0; i <= 200; i++)
+    {
+        // el char * es la manera de acceder a los bytes dentro de un fichero, le indica una posicion en la memorio con el &numeroLeido (donde se va a almacenar), 
+        // despues hay que indicarle el tamaño del dato con el tipo entre parentesis
+        fin.read((char *) &numeroLeido, sizeof(int));
+
+        //Mostramos por pantalla lo que hemos almacenado en numero leido
+        cout << numeroLeido << endl;
+    }    
+    fin.close();//Cerramos el fichero despues trabajar con el 
+
+    return 0;
+}
