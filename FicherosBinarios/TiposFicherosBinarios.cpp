83c6bb33314fd7f350ed74ff48cc076fa057dc50
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2023-07-03 12:56:14 +0200

    añado una funcion para escribir en imagenes

--- FicherosBinarios/TiposFicherosBinarios.cpp
+++ FicherosBinarios/TiposFicherosBinarios.cpp
@@ -52,6 +52,38 @@ string deducirTipoFichero(string ruta)
     return tipoFichero;
 }
 
+string escribirImagen (string ruta, string frase)
+{
+    //Variables
+    char letra = '0';
+    char letraLeida = '0';
+    int i = 0;
+
+    //Para abrir el fichero en modo de escritura usamos fstream, si el fichero no existe lo va a crear, y el app es para que no machaque la imagen
+    fstream fout(ruta, ios::binary | ios::out | ios::app);
+
+    if (fout.is_open())
+    {
+        //si el fichero se abre
+        //Creamos un bucle para que esriba los numeros dentro del fichero
+        for ( i = 0; i <= frase.length(); i++)
+        {
+            // el char * es la manera de acceder a los bytes dentro de un fichero, le indica una posicion en la memorio con el &i, 
+            // despues hay que indicarle el tamaño del dato con el tipo entre parentesis
+            fout.write((char *) &frase[i], sizeof(char));
+        }
+        fout.close(); //Hay que cerrar el fichero como siempre que trabajamos con ficheros
+        cout << "Texto camuflado correctamente" << endl;
+    }
+    else
+    {   
+        //Si el fichero no se abre
+        cout << "El fichero no se puede abrir." << endl;
+    }
+
+    return frase;
+}
+
 
 
 
@@ -62,6 +94,9 @@ int main()
     string ruta = "";
     int opcion = 0;
     string tipoFichero = "";
+    string frase = "";
+    string resultado = "";
+
  
 
     do
@@ -71,6 +106,7 @@ int main()
         cout << "|----------------------------------------------|" << endl;
         cout << "|0.- Salir del programa                        |" << endl;
         cout << "|1.- Deducir tipo de fichero                   |" << endl;
+        cout << "|2.- Escribir en una imagen                    |" << endl;
         cout << "|----------------------------------------------|" << endl;
         cout << "|Seleccione una opcion:                        |" << endl;
         cin >> opcion;
@@ -99,6 +135,18 @@ int main()
                 cout << "El tipo de fichero es: " << tipoFichero <<endl;
                 cout << endl; //Mayor legibilidad
             }
+            break;
+        case 2:
+            //Escribir en una imagen
+            //Pedimos al usuario la frase y la ruta
+            cout << "Introduce la frase que quieres camuflar en la imagen" << endl;
+            getline(cin,frase);//Este es para capturar el intro
+            getline(cin, frase);
+            cout << "Introduce la ruta de la imagen" << endl;
+            cin >> ruta;
+
+            //Capturamos lo que devuelve la funcion
+            resultado = escribirImagen(ruta, frase); 
             break;
         
         default:
