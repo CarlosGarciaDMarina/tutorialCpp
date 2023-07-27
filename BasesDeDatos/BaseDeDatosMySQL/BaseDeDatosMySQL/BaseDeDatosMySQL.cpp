46985c0ab4871f0bcccd9e621a5c5059e00b5260
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2023-07-27 16:27:33 +0200

    Acabamos el tutorial de mysql

--- BasesDeDatos/BaseDeDatosMySQL/BaseDeDatosMySQL/BaseDeDatosMySQL.cpp
+++ BasesDeDatos/BaseDeDatosMySQL/BaseDeDatosMySQL/BaseDeDatosMySQL.cpp
@@ -12,6 +12,10 @@ int main()
     MYSQL_RES* res; //Obtener registros
     MYSQL_ROW row; //Me permite acceder a los campos de cada uno de los registros
 
+    string consulta = "";
+    const char* c; //Convertiremos el string de conexion en este tipo de dato, nos obliga el programa
+    int result = 0;
+
     conectar = mysql_init(0);
 
     if (!mysql_real_connect(conectar, "localhost", "root", "Root", "basedatoscplus", 3306, NULL, 0))
@@ -21,6 +25,78 @@ int main()
     else
     {
         cout << "Se ha establecido la conexion con exito." << endl;
+
+        /* INSERT*/
+        //Hacemos la consulta, convertimos la consulta en char* y luego mysql_query es la funcion para hacer una consulta en la base de datos(conexion, tipo de dato) 
+        consulta = "INSERT INTO usuarios (nombre, edad) VALUES ('Gura', 10)";
+        c = consulta.c_str();
+        result = mysql_query(conectar, c);
+
+        if (!result)
+        {
+            cout << "Insercion correcta" << endl;
+        }
+        else
+        {
+            cout << "Ha ocurrido un error al insertar un dato" << endl;
+        }
+
+        /*UPDATE*/
+        consulta = "UPDATE usuarios SET edad = 13 WHERE id = 5";
+        c = consulta.c_str();
+        result = mysql_query(conectar, c);
+
+        if (!result)
+        {
+            cout << "Actualizacion correcta" << endl;
+        }
+        else
+        {
+            cout << "Ha ocurrido un error al actualizar un dato" << endl;
+        }
+
+        /* DELETE */
+        consulta = "DELETE FROM usuarios WHERE id = 3";
+        c = consulta.c_str();
+        result = mysql_query(conectar, c);
+
+        if (!result)
+        {
+            cout << "El dato ha sido borrado con exito" << endl;
+        }
+        else
+        {
+            cout << "Ha ocurrido un error al borrar un dato" << endl;
+        }
+
+        /* SELECT */
+        consulta = "SELECT * FROM usuarios";
+        c = consulta.c_str();
+        result = mysql_query(conectar, c);
+
+        if (!result)
+        {
+            cout << "Mostrando los datos recibidos: " << endl;
+            cout << endl;
+            res = mysql_store_result(conectar);//Con esto vamos a obtener un array de registros
+
+            //Bucle while para ir recogiendolos
+            while (row=mysql_fetch_row(res)) //ROW se encarga de separarlos por campos, los divide en columnas y luego tenemos que llamarlo de manera independiente
+            {
+                cout << "ID: " << row[0] << " - " << "Nombre: " << row[1] << " - " << "Edad: " << row[2] << "." << endl;
+            }
+        }
+        else
+        {
+            cout << "Ha ocurrido un error al recibir un dato" << endl;
+        }
+
+        /* Cuando terminemos las consultas SIEMPRE cerramos la conexion */
+        cout << endl;
+        cout << "Cerramos la conexion " << endl;
+        mysql_close(conectar);
+
+
     }
 
 
